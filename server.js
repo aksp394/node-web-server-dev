faca5eb5c8103005f394cd23f963cd8f6fabc2d4
Arun <Arun.patil125@gmail.com>
2019-01-24 09:39:20 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Express!!');
+});
+
+app.get('/about', (req, res) =>{
+ res.render('about.hbs', {
+     welcomeMessage : 'About page'
+ });
+});
+
+app.get('/home', (req, res) => {
+ res.render('home.hbs', {
+    welcomeMessage : 'Welcome Arun'
+ })
+});
+
+app.listen(3000);
