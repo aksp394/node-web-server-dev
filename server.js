8a1cb894826c7314bd532a25d1057faa5a608004
Arun <Arun.patil125@gmail.com>
2019-01-25 09:41:50 +0530

    server setup

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const hbs = require('hbs');
+var port = process.env.PORT || 3000;
 
 var app = express();
 
-const port = process.env.PORT || 3000;
+
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
